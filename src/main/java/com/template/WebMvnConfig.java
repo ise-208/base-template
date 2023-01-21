ff35e5e1cc1d513d83f5af6dd1fcce4271a510cb
--global <seed.0ri00@gmail.com>
2023-01-20 21:49:43 -0800

    Configの作成

---
+++ src/main/java/com/template/WebMvnConfig.java
@@ -0,0 +1,13 @@
+package com.template;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+@Configuration
+public class WebMvnConfig {
+
+    public void addVierController(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
