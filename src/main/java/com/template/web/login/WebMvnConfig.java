ed06544fa201871c52a09783f755ae2afa1b129e
--global <seed.0ri00@gmail.com>
2023-01-21 20:02:21 -0800

    画面の作成

--- src/main/java/com/template/web/login/WebMvnConfig.java
+++ src/main/java/com/template/web/login/WebMvnConfig.java
@@ -1,4 +1,4 @@
-package com.template;
+package com.template.web.login;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
