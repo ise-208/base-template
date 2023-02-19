364e88e3b162db7e6a58bb639ec33644435f624b
--global <seed.0ri00@gmail.com>
2023-02-19 22:28:01 +0900

    サービス層の実装

--- src/main/java/com/template/web/login/AccountService.java
+++ src/main/java/com/template/web/login/AccountService.java
@@ -1,12 +1,22 @@
 package com.template.web.login;
 
+import lombok.val;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class AccountService {
-    public WebUser create(WebUser user, String password){
-        return null;
+    @Autowired
+    AccountRepository accountRepository;
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public WebUser create(WebUser user, String password) {
+        String encode = passwordEncoder.encode(password);
+        user.setPassword(encode);
+        return accountRepository.save(user);
     }
 }
