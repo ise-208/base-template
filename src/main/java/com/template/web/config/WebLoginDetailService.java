9238db85d3d3a1e778103b6bf43c94b89555ddd2
--global <seed.0ri00@gmail.com>
2023-05-22 23:57:53 +0900

    userrepositoryの検証

--- src/main/java/com/template/web/config/WebLoginDetailService.java
+++ src/main/java/com/template/web/config/WebLoginDetailService.java
@@ -11,13 +11,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class WebLoginDetailService implements UserDetailsService {
-    @Autowired
+
     private WebUserMapperRepository webUserMapperRepository;
+
+    public WebLoginDetailService(WebUserMapperRepository webUserMapperRepository) {
+        this.webUserMapperRepository = webUserMapperRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String id) {
-        Account account = webUserMapperRepository.findById(id);
-
+//        Account account = webUserMapperRepository.findById(id);
+        Account account = new Account();
+        account.setId("user1");
+        account.setName("one");
+        account.setPassword("password");
+        account.setRole("ROLE_USER");
         return User.withUsername(account.getUsername())
                 .password(account.getPassword())
                 .roles(account.getRole())
--- src/main/java/com/template/web/repository/WebUserMapperRepository.java
+++ src/main/java/com/template/web/repository/WebUserMapperRepository.java
@@ -3,14 +3,20 @@ package com.template.web.repository;
 import com.template.web.entity.Account;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcDaoSupport;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
-public interface WebUserMapperRepository {
-//    public Collection<User> findAll();
+public class WebUserMapperRepository {
+    //    public Collection<User> findAll();
     @Select("select * from users where id = #{id}")
-    Account findById(String id);
+    Account findById(String id) {
+        return null;
+    }
+
 //    public void create(User user);
 //    public void saveUser(User user);
 //    public List<User> findAllUsers();
